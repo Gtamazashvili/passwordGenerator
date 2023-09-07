f8dd81111ac88cc7e428a12f649ac3de051ba69a
Gtamazashvili <118844834+Gtamazashvili@users.noreply.github.com>
2023-09-07 05:30:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
+    const keyboardSymbols = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
+            '_', '=', '+', '[', '{', ']', '}', , '|', ';', ':', '\'', '"',
+            ',', '<', '.', '>', '/', '?', ' '
+    ];
+
     document.getElementById("myRange").oninput = function() {
         document.getElementById("numberOfChars").innerHTML = this.value;
     };
-});
-let number=parseInt(document.getElementById("numberOfChars"));
-const keyboardSymbols = [
-    
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
-    '_', '=', '+', '[', '{', ']', '}', '\\', '|', ';', ':', '\'', '"',
-    ',', '<', '.', '>', '/', '?', ' '
-];document.getElementById("generate").addEventListener("click", function() {
-    let password="";
-    for(let i = 0; i < number.length; i++) {
-        let index=Math.random(0,keyboardSymbols.length)
-        password+=keyboardSymbols[index];
-    }
-    console.log(password);
+
+    document.getElementById("generate").addEventListener("click", function() {
+        let number = parseInt(document.getElementById("numberOfChars").innerText);
+        let password = "";
+        
+        for(let i = 0; i < number; i++) {
+            let index = Math.floor(Math.random() * keyboardSymbols.length);
+            password += keyboardSymbols[index];
+        }
+        
+        document.querySelector(".password").value = password;
+    });
+    document.getElementById("copy").addEventListener("click",function(){
+        var copyText= document.getElementById("password");
+        copyText.select();
+        document.execCommand("copy");
+        copyText.setSelectionRange(copyText.value.length, copyText.value.length);
+    })
 });
